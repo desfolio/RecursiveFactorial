81882e9f124f8a0bef5065e1c6c1f5dc9fdf20cf
Desmond Williams <desmond@desfolio.com>
2020-05-13 15:08:47 -0400

    minor spacing in user prompt

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RecursiveFactorial
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number to find the factorial of it:");
+            Console.Write("Enter a number to find the factorial of it: ");
             var num = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"The facotrial of {num} is {Factorial(num)}. ");
